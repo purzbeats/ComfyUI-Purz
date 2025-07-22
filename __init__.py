70739cc389bc4c98bb90d674566db41a33d400ad
Purz <97489706+purzbeats@users.noreply.github.com>
2025-07-22 17:13:32 -0400

    fix

--- __init__.py
+++ __init__.py
@@ -2,7 +2,6 @@
 ComfyUI-Purz Custom Node Pack
 """
 
-from .version import __version__
 from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
-__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "__version__"]
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
